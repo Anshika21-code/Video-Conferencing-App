ecc003110c777bca16ee0e829c7ec6f44be767e7
Anshika Tank <anshikatank2110@gmail.com>
2026-02-11 19:56:09 +0530

    Light Dark Toggle button set

--- app/Providers.js
+++ app/Providers.js
@@ -18,6 +18,30 @@
 //   )
 // }
    
+// "use client";
+
+// import { SessionProvider } from "next-auth/react";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { ThemeProvider } from "next-themes";
+
+// export function Providers({ children, session }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <ToastContainer position="top-right" autoClose={3000} />
+//       <ThemeProvider
+//         attribute="class"
+//         defaultTheme="light"
+//         enableSystem={false}
+         
+//       >
+//         {children}
+//       </ThemeProvider>
+//     </SessionProvider>
+//   );
+// }
+
+
 "use client";
 
 import { SessionProvider } from "next-auth/react";
@@ -27,16 +51,11 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children, session }) {
   return (
-    <SessionProvider session={session}>
-      <ToastContainer position="top-right" autoClose={3000} />
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem={false}
-        forcedTheme="light"
-      >
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <SessionProvider session={session}>
+        <ToastContainer position="top-right" autoClose={3000} />
         {children}
-      </ThemeProvider>
-    </SessionProvider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,14 +1,39 @@
-import React, { useState } from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Sun, Moon } from 'lucide-react';
 
 
 const Header = () => {
     const {theme, setTheme} = useTheme();
     const {data:session,status} = useSession();
     const [open,setOpen]= useState(false);
+    const formatTimeDate = (date) => {
+        const now = new Date();
+        return now.toLocaleString("en-US", {
+            hour:'numeric',
+            minute:'numeric',
+            hour12:true,
+            weekday:'short',
+            month:'short',
+            day:'numeric',
+            year:'numeric'
+        })
+    }
+
+    const [mounted, setMounted] = useState(false);
+        
+        useEffect(() => {
+          setMounted(true);
+        }, []);
+        
+    if (!mounted) return null;
+
 
 
   return (
@@ -29,9 +54,20 @@ const Header = () => {
         {/* <span className='hidden md:block text-xl font-semibold text-gray-800 dark:text-white'> 
 
         </span> */}
+      </div>
+      <div className='flex items-center space-x-4'>
+        <span className="text-md text-gray-500 dark:text-gray-200">
+            {formatTimeDate(new Date())}
 
-        
+        </span>
+        <Button variant="ghost" onClick={()=> setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'dark' ? (
+            <Sun className="w-5 h-5 text-yellow-400"></Sun> 
+            ) : (
+            <Moon className="w-5 h-5 text-gray-800"></Moon>
 
+            )}
+        </Button>
       </div>
     </div>
   )
